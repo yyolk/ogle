176c8c255de9dc79345adb46e3d2ea62ff62e0f9
Joseph Yølk Chiocchi <joe@yolk.cc>
2015-07-22 20:50:39 -0500

    init package

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log('Loading function');
+var qs = require('qs');
+var request = require('request');
+var jsonPath = require('JSONPath');
+
+
+
+exports.handler = function(event, context) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('Received context:', JSON.stringify(context, null, 2));
+
+    // var proxy = function(event) {
+
+    // };
+
+    switch (event.action){
+        case 'create':
+            return proxy(event.appointment);
+            break;
+        default:
+            return context.fail(new Error("Error: Invalid path."));
+            break;
+
+    };
+};
